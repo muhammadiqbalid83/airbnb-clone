265701f59c5c76a4241a06acc415a3024d720dc7
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-10-02 08:32:15 +0700

    add navbar

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+import { FC } from "react";
+
+interface RegisterModalProps {}
+
+const RegisterModal: FC<RegisterModalProps> = ({}) => {
+  const registerModal = useRegisterModal();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  return <div></div>;
+};
+
+export default RegisterModal;
